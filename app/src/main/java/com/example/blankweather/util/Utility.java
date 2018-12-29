d7c84cd95cf9d3fecffcb8b1f422bc1f5d904201
Zhangfutao <zft_ccc@163.com>
2018-12-29 11:10:21 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/blankweather/util/Utility.java
+++ app/src/main/java/com/example/blankweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.blankweather.db.City;
 import com.example.blankweather.db.County;
 import com.example.blankweather.db.Province;
+import com.example.blankweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -88,6 +90,21 @@ public class Utility {
 
         }
         return false;
+    }
+
+
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
     }
 
 }
